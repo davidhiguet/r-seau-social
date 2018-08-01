9672dccd917e8db05e7fd6a23645bcce6d9870fa
davidhiguet <38644243+davidhiguet@users.noreply.github.com>
2018-08-01 23:42:44 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,13 @@ var router = express.Router(),
   User = require("../bdd/user");
 
 router.get("/", function(req, res, next) {
-  var globalMsg;
-  var globalCnt;
+ 
   Promise.all([
     User.find({ connected: { $ne: "notconnected" } }).count().then(number => {
-        globalCnt = number;
+      globalProperty = number;
     }), 
-    User.find("messag").then(number => {
-      let nbMsg = 0
-
-      number.map(nb => {
-        
-        if (nb.messag.length != 0) {
-          nbMsg = nbMsg + Number(nb.messag.length);
-          
-          console.log("taba", nbMsg);
-          
-        }
-        globalMsg = nbMsg;
-      });
-    })
   ]).then(()=> {
     
-    let globalProperty= {};
-    globalProperty.messages = globalMsg;
-    globalProperty.connected = globalCnt;
     console.log("taba", globalProperty);
     
     return res.render("index", { globalProperty, auth: true});
